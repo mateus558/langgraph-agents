e593cefad95509ea5d95949a97c7a427ff964022
Mateus Coutinho Marim <mateusmarim@trainwithmeapp.com>
2025-11-02 12:18:13 -0300

    up

--- scripts/test_api.py
+++ scripts/test_api.py
@@ -4,10 +4,10 @@ from langgraph_sdk import get_client
 async def main():
     my_token = "123" # In practice, you would generate a signed token with your auth provider
     client = get_client(
-        url="http://localhost:2024",
+        url="http://192.168.30.100:8123",
         api_key=my_token,
     )
-    threads = await client.threads.search()
+    threads = await client.assistants.search()
     return threads
 
 if __name__ == "__main__":
